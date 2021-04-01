ecf00f0b5a792f4f434c82662c7eebcd97a690c8
kmtszky <kmtszk711@gmail.com>
2021-04-01 07:40:38 +0000

    [fix] users, books controller修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,8 +24,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-
-
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -35,7 +33,7 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
     @book.destoy
     redirect_to books_path
@@ -44,7 +42,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
